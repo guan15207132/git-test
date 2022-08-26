909cf3ce3841fbe6d2948ae799daef3da0a154ee
wuqiguan <wqg152@atguigu.com>
2022-08-27 00:13:37 +0800

    hot-fixtwo commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("这是版本修改后的内容！");
         System.out.println("在hot-fix中修改分支中的内容！");
+        System.out.println("在hot-fix中修改分支中的内容再次修改！");
     }
 }
