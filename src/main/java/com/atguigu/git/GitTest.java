a9995f631c81b7a9dafa783da521916944b8640d
wuqiguan <wqg152@atguigu.com>
2022-08-27 00:53:54 +0800

    Merge branch 'hot-fix'
    # Conflicts:
    #	src/main/java/com/atguigu/git/GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("这是master分支中的内容！");
         System.out.println("即将开始合并分支....");
         System.out.println("这是wqg-fix分支中的内容！");
+        System.out.println("即将将项目推送到远程库中");
     }
 }
