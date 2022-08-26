b0b67cf5b09b6e854b86f4a5cd62e97b0bec11c5
guan15207132 <107193359+guan15207132@users.noreply.github.com>
2022-08-27 01:03:27 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("即将开始合并分支....");
         System.out.println("这是wqg-fix分支中的内容！");
         System.out.println("即将将项目推送到远程库中");
+           System.out.println("手动修改内容,待录取...");
+           System.out.println("东方不败！");
     }
 }
