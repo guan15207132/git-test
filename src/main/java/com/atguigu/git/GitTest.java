bfe165ca148e65c84c13dd9bfcdae4f7a57cb818
wuqiguan <wqg152@atguigu.com>
2022-08-27 00:25:35 +0800

    Merge branch 'hot-fix'
    # Conflicts:
    #	src/main/java/com/atguigu/git/GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("这是版本修改后的内容！");
         System.out.println("在hot-fix分支中修改分支中的内容！");
-        System.out.println("修改的内容！");
+        System.out.println("这是abc-fix分支中的内容！");
     }
 }
