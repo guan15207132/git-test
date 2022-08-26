af81374382412cd1da2301ec42552c9483ecbe47
wuqiguan <wqg152@atguigu.com>
2022-08-27 00:12:42 +0800

    master commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("这是版本修改后的内容！");
-        System.out.println("在hot-fix中修改分支中的内容！");
+        System.out.println("这是合并前的mastee分支的内容");
     }
 }
